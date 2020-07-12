232c8844ddb696f2bbb884c9133bf7b2ed8e5193
shigimcp <shigimcp@hotmail.com>
2020-07-12 13:51:09 -0400

    Updates

---
+++ precache-manifest.490be47780dbf89dcfae56a75dbda860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac58a92394ca277a165e489a34e81ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db953fbf13175d211cc7",
+    "url": "/static/css/main.f5d59821.chunk.css"
+  },
+  {
+    "revision": "91e6049429a97d8d2272",
+    "url": "/static/js/2.f91a1bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f91a1bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db953fbf13175d211cc7",
+    "url": "/static/js/main.6e5a3676.chunk.js"
+  },
+  {
+    "revision": "31f343e97f7635d7e0df",
+    "url": "/static/js/runtime-main.9d670c71.js"
+  }
+]);
