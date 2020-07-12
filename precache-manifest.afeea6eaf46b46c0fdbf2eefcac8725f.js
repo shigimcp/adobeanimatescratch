2b868b1f0d5ac88652248bad80b0b3311f10abb7
shigimcp <shigimcp@hotmail.com>
2020-07-12 01:47:24 -0400

    Updates

---
+++ precache-manifest.afeea6eaf46b46c0fdbf2eefcac8725f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387562bddb69ac5a0a365cf6e110adf3",
+    "url": "/adobeanimatescratch/index.html"
+  },
+  {
+    "revision": "8bc11562e78342a620a1",
+    "url": "/adobeanimatescratch/static/css/main.f5d59821.chunk.css"
+  },
+  {
+    "revision": "91e6049429a97d8d2272",
+    "url": "/adobeanimatescratch/static/js/2.f91a1bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adobeanimatescratch/static/js/2.f91a1bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc11562e78342a620a1",
+    "url": "/adobeanimatescratch/static/js/main.33f04d9d.chunk.js"
+  },
+  {
+    "revision": "ec178d7dee8b85339180",
+    "url": "/adobeanimatescratch/static/js/runtime-main.a7ced899.js"
+  }
+]);
